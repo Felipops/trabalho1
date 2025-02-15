f98dbb0121e28aab590b0e9f24777db8cfa4a2d0
asus <asus@LAPTOP-SLUIFDPR>
2025-02-15 17:32:52 -0300

    primeiro commit

---
+++ trabalho1/src/main/java/app/service/AlunoService.java
@@ -0,0 +1,41 @@
+package app.service;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.entity.Aluno;
+import app.repository.AlunoRepository;
+
+@Service
+public class AlunoService {
+	
+	@Autowired
+	private AlunoRepository alunoRepository;
+	
+	public String save(Aluno aluno) {
+		this.alunoRepository.save(aluno);
+		return "Aluno salvo com sucesso!";
+	}
+	
+	public String delete(long id) {
+		this.alunoRepository.deleteById(id);
+		return "Aluno deletado com sucesso!";
+	}
+	
+	public Aluno findById(long id) {
+		return this.alunoRepository.findById(id).get();
+	}
+	
+	public String update(long id, Aluno aluno) {
+		aluno.setId(id);
+		this.alunoRepository.save(aluno);
+		return "Aluno foi atualizado com sucesso!";
+	}
+	
+	public List<Aluno> findAll(){
+		return this.alunoRepository.findAll();
+	}
+}
